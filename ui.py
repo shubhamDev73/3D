9d81e491dbb085000467ee8dfd906e2ea354d71a
shubhamDev73 <shubham0209@gmail.com>
2020-03-30 08:31:58 +0530

    Added basic propertiesBar

--- ui.py
+++ ui.py
@@ -11,15 +11,15 @@ defaultColor = "black"
 def setup():
 	""" Setting up the UI to be used """
 
-	# configuring UI style
-	style = Style()
-	style.configure("TButton", foreground="red", background="white")
-	style.configure("TEntry", foreground="red", background="white")
-
 	# creating global variables as they are used by other functions too
 	global root, canvas, menuBar, propertiesBar
 
 	root = Tk()
+
+	# configuring UI style
+	style = Style()
+	style.configure("TButton", foreground="red", background="white")
+	style.configure("TEntry", foreground="red", background="white")
 
 	# different frames
 	menuBar = Frame(root)
@@ -39,6 +39,59 @@ def setup():
 	Button(menuBar, text="Edit", command=edit).pack(side="left")
 	Button(menuBar, text="Export", command=export).pack(side="left")
 	Button(menuBar, text="Render", command=None).pack(side="left")
+
+	# creating properties bar
+
+	# position
+	position = Frame(propertiesBar)
+	position.pack(side="top")
+	Label(position, text="Position").pack(side="top")
+	x = Frame(position)
+	x.pack(side="top")
+	Label(x, text="X: ").pack(side="left")
+	Entry(x).pack(side="right")
+	y = Frame(position)
+	y.pack(side="top")
+	Label(y, text="Y: ").pack(side="left")
+	Entry(y).pack(side="right")
+	z = Frame(position)
+	z.pack(side="top")
+	Label(z, text="Z: ").pack(side="left")
+	Entry(z).pack(side="right")
+
+	# rotation
+	rotation = Frame(propertiesBar)
+	rotation.pack(side="top")
+	Label(rotation, text="Rotation").pack(side="top")
+	x = Frame(rotation)
+	x.pack(side="top")
+	Label(x, text="X: ").pack(side="left")
+	Entry(x).pack(side="right")
+	y = Frame(rotation)
+	y.pack(side="top")
+	Label(y, text="Y: ").pack(side="left")
+	Entry(y).pack(side="right")
+	z = Frame(rotation)
+	z.pack(side="top")
+	Label(z, text="Z: ").pack(side="left")
+	Entry(z).pack(side="right")
+
+	# scale
+	scale = Frame(propertiesBar)
+	scale.pack(side="top")
+	Label(scale, text="Scale").pack(side="top")
+	x = Frame(scale)
+	x.pack(side="top")
+	Label(x, text="X: ").pack(side="left")
+	Entry(x).pack(side="right")
+	y = Frame(scale)
+	y.pack(side="top")
+	Label(y, text="Y: ").pack(side="left")
+	Entry(y).pack(side="right")
+	z = Frame(scale)
+	z.pack(side="top")
+	Label(z, text="Z: ").pack(side="left")
+	Entry(z).pack(side="right")
 
 def clear():
 	canvas.delete("all")
